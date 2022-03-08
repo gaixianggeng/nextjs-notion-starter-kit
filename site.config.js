651756de8822a030ee90417b3c97f96a140d5abc
gai <gaih@outlook.com>
2022-03-08 19:46:19 +0800

    change config

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'gai',
+  domain: 'gaixg.com',
+  author: 'gai',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'gaixiaotongxue',
+  github: 'gaixianggeng',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
